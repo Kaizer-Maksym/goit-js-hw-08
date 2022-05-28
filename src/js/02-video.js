b04bfb61dbc1fd555d19bd543aab376ea2af732d
Kaizer Maksym <kaizeriimaxim@gmail.com>
2022-05-29 00:27:49 +0200

    2-hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  data = {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+  };
+
+  setCurrentTime();
+}
+
+function setCurrentTime() {
+  player.getCurrentTime().then(function (seconds) {
+    time = seconds;
+    return time;
+  });
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
