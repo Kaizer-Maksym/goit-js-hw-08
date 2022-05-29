d83d4ac498a07183ec2bc5a50340cf86c7799b03
Kaizer Maksym <kaizeriimaxim@gmail.com>
2022-05-29 18:12:15 +0200

    3-hw-fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,57 +2,38 @@ import throttle from 'lodash.throttle';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  inputEmail: document.querySelector('label input'),
-  textarea: document.querySelector('label textarea'),
+  input: document.querySelector(' input'),
+  textarea: document.querySelector(' textarea'),
 };
-const EMAIL_KEY = 'feedback-email';
-const TEXTAREA_KEY = 'feedback-massage';
 
-completionOfEmail();
-completionOfTextarea();
+const STORAGE_KEY = 'feedback-form-state';
 
+refs.form.addEventListener('input', throttle(changeInputValue, 500));
 refs.form.addEventListener('submit', onFormSubmit);
-refs.inputEmail.addEventListener('input', throttle(changeInputValue, 500));
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+function changeInputValue(e) {
+  const dataForm = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+}
+
+function completionForm() {
+  const formValue = JSON.parse(localStorage.getItem(STORAGE_KEY)) || '';
+  if (formValue) {
+    refs.input.value = formValue.email;
+    refs.textarea.value = formValue.message;
+  }
+}
+completionForm();
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  const data = {
-    email: localStorage.getItem(EMAIL_KEY),
-    message: localStorage.getItem(TEXTAREA_KEY),
-  };
+  console.log(localStorage.getItem(STORAGE_KEY));
 
-  console.log(data);
-
-  localStorage.removeItem(EMAIL_KEY);
-  localStorage.removeItem(TEXTAREA_KEY);
+  localStorage.removeItem(STORAGE_KEY);
 
   e.currentTarget.reset();
-}
-
-function changeInputValue(e) {
-  const email = e.target.value;
-  localStorage.setItem(EMAIL_KEY, email);
-}
-
-function onTextareaInput(e) {
-  const message = e.target.value;
-  localStorage.setItem(TEXTAREA_KEY, message);
-}
-
-function completionOfEmail() {
-  const saveEmail = localStorage.getItem(EMAIL_KEY);
-
-  if (saveEmail) {
-    refs.inputEmail.value = saveEmail;
-  }
-}
-
-function completionOfTextarea() {
-  const saveTextareaValue = localStorage.getItem(TEXTAREA_KEY);
-
-  if (saveTextareaValue) {
-    refs.textarea.value = saveTextareaValue;
-  }
 }
